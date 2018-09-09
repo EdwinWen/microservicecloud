352e5fbea9552de12a92ae5d19bf7a3d9cd2cd5c
EdwinWen <zwp92066@163.com>
2018-09-09 13:45:43 +0800

    第三次

--- microservicecloud/microservicecloudproviderdept8001/src/main/java/com/edwin/springcloud/controller/DeptController.java
+++ microservicecloud/microservicecloudproviderdept8001/src/main/java/com/edwin/springcloud/controller/DeptController.java
@@ -14,7 +14,7 @@ public class DeptController {
 
     @RequestMapping(value="dept/get/{id}",method= RequestMethod.GET)
     public String get(@PathVariable("id") String id) {
-        System.out.println("deptcontroller:--2222--" + id);
+        System.out.println("deptcontrollaaaer:--edwin00--" + id);
         return "hello,world";
 
     }
