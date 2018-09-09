df1fc3b783434edd8d700a6edb68847a476930bd
EdwinWen <zwp92066@163.com>
2018-09-09 18:46:02 +0800

    555

---
+++ microservicecloud/microservicecloud/microservicecloudproviderdept8001/src/main/java/com/edwin/springcloud/dao/DpetDao.java
@@ -0,0 +1,7 @@
+package com.edwin.springcloud.dao;
+
+/**
+ * Created by wenpuzhao on 2018/9/9.
+ */
+public class DpetDao {
+}
---
+++ microservicecloud/microservicecloud/microservicecloudproviderdept8001/src/main/java/com/edwin/springcloud/service/DeptService.java
@@ -0,0 +1,7 @@
+package com.edwin.springcloud.service;
+
+/**
+ * Created by wenpuzhao on 2018/9/9.
+ */
+public interface DeptService {
+}
