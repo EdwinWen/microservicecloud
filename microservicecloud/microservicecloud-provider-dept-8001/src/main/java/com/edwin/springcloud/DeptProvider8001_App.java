e3cbd8dccadeb90a3d4e0afce1dd7c3c3ae9fecb
EdwinWen <zwp92066@163.com>
2018-11-07 14:47:11 +0800

    添加定时任务--DeptProvider8001_app

--- microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/edwin/springcloud/DeptProvider8001_App.java
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/edwin/springcloud/DeptProvider8001_App.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by wenpuzhao on 2018/9/9.
  */
 
+
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 @EnableDiscoveryClient //服务发现
+@EnableScheduling // 开启基于注解的定时任务
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
---
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/edwin/springcloud/service/impl/ScheduleService.java
@@ -0,0 +1,21 @@
+package com.edwin.springcloud.service.impl;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by wenpuzhao on 2018/11/7.
+ */
+@Service
+public class ScheduleService {
+
+    /**
+    *   second (秒), minute（分）, hour （小时）, day of month（日期）, month（月份）, day of week（周几）
+    *   例子："0 * * * * MON-FRI"
+     *
+    * */
+    @Scheduled(cron ="0 0/1 * * * ?") // 每隔1分钟执行一次
+    public void hello(){
+        System.out.println("hello...");
+    }
+}
