0ad460a8002d9031d1e23afe1777511071705c6f
EdwinWen <zwp92066@163.com>
2018-11-16 14:44:57 +0800

    添加定时任务--DeptProvider8001_app

--- microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/edwin/springcloud/service/impl/ScheduleService.java
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/edwin/springcloud/service/impl/ScheduleService.java
@@ -13,9 +13,10 @@ public class ScheduleService {
     *   second (秒), minute（分）, hour （小时）, day of month（日期）, month（月份）, day of week（周几）
     *   例子："0 * * * * MON-FRI"
      *
+     *  每隔1分钟去查看服务是否连接正常
     * */
     @Scheduled(cron ="0 0/1 * * * ?") // 每隔1分钟执行一次
-    public void hello(){
+    public void ScheduleHealth(){
         System.out.println("hello...");
     }
 }
